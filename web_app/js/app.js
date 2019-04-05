40a301981730d082003f70530fbe5182e50ec10c
jennarchibald <codeclan@CodeClans-MacBook-Pro.local>
2019-04-05 14:34:34 +0100

    css changes

--- web_app/js/app.js
+++ web_app/js/app.js
@@ -2,27 +2,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
   console.log('script loaded');
 
+  // adding a list item
+  //
+  //
+
   const addItemForm = document.querySelector('.add-item-form');
 
   const handleAddItemFormSubmit = function(evt){
     evt.preventDefault()
 
+    // this function picks out values from the event
     const valueFinder = function(input){
         return evt.target[input].value
     };
 
+    // this creates a new li element and sets the text content
     const listItem = document.createElement('li');
     listItem.textContent = `You need to buy ${valueFinder('quantity')} ${valueFinder('item')} ${valueFinder('shop')} ${valueFinder('urgency')}`;
 
+  // this adds the list item to the shopping list
     const shoppingList = document.querySelector('.list-display');
     shoppingList.appendChild(listItem);
 
+    // this resets the form fields to empty
     const form = document.querySelector('form.add-item-form');
     form.reset();
   };
 
   addItemForm.addEventListener('submit', handleAddItemFormSubmit);
 
+  // deleting all list items
+  //
+  // 
 
   const deleteAllButton = document.querySelector('#delete-all-button');
 
